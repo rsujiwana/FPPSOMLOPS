0fed5c614e5646745d7dd3233ac77a002a9177ac
rsujiwana <113880876+rsujiwana@users.noreply.github.com>
2024-06-12 01:47:37 +0700

    Update test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -3,5 +3,5 @@ from app.utils import labeling_function
 
 def test_labeling_function():
     input_data = "This is a sample input text."
-    expected_output = {"label": "Expected label"}  # Sesuaikan dengan output yang diharapkan
-    assert labeling_function(input_data) == expected_output
+    expected_confidence = 0.90
+    assert labeling_function(input_data)[0]["confidence"][0] >= expected_confidence
