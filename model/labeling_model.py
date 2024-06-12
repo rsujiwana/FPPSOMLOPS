60cbb9099cff13540e423e7b497702bde728fe85
ndtsaniyah <tsaniyah.coshack1019@gmail.com>
2024-06-12 16:12:34 +0700

    Perbaikan code yang mana result akan disimpan pada file JSON

--- model/labeling_model.py
+++ model/labeling_model.py
@@ -8,11 +8,17 @@ aspect_extractor = ATEPC.AspectExtractor('multilingual',
                                          cal_perplexity=True,
                                          )
 
-def label_text(text):
+def label_data(text):
     # Implementasi pelabelan dengan model yang sudah dilatih
     result = aspect_extractor.predict([text],
                          save_result=True,
                          print_result=True,  # print the result
                          ignore_error=True,  # ignore the error when the model cannot predict the input
                          )
+
+    output_file = "result.json"
+    with open(output_file, 'w', encoding='utf-8') as f:
+        json.dump(result, f, ensure_ascii=False, indent=4)
+        
     return result
+
