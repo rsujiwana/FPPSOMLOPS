5b1bcb7e75e773fce2e916bffe8df00da9b58646
ndtsaniyah <90408425+ndtsaniyah@users.noreply.github.com>
2024-06-13 13:47:40 +0700

    Update labeling_model.py

--- model/labeling_model.py
+++ model/labeling_model.py
@@ -13,24 +13,9 @@ aspect_extractor = ATEPC.AspectExtractor('multilingual',
 def label_text(text):
     # Implementasi pelabelan dengan model yang sudah dilatih
     result = aspect_extractor.predict([text],
-                         save_result=False,
+                         save_result=True,
                          print_result=True,  # print the result
                          ignore_error=True,  # ignore the error when the model cannot predict the input
                          )
-
-    # Load existing results from JSON file if it exists
-    output_file = "result.json"
-    if os.path.exists(output_file):
-        with open(output_file, 'r', encoding='utf-8') as f:
-            existing_results = json.load(f)
-    else:
-        existing_results = []
-
-    # Append new result to existing results
-    existing_results.append(result)
-
-    # Save the updated results back to JSON file
-    with open(output_file, 'w', encoding='utf-8') as f:
-        json.dump(existing_results, f, ensure_ascii=False, indent=4)
     return result
 
