e4438e323f4a707b06f64db3544164d060b9fc55
rsujiwana <113880876+rsujiwana@users.noreply.github.com>
2024-06-20 06:27:55 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,12 @@ set -o nounset
 
 /usr/local/bin/uwsgi --http 0.0.0.0:5000 \
     --plugins python3 \
-    --uwsgi uwsgi.ini
+    --wsgi run:app \
+    --master true \
+    --processes 1 \
+    --socket :5000 \
+    --vacuum true \
+    --die-on-term true \
 
 echo ""
 echo "$@"
