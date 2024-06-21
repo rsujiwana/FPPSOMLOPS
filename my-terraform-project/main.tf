205478abdb12da45d4ded53fc5751d42dd2a2658
ndtsaniyah <90408425+ndtsaniyah@users.noreply.github.com>
2024-06-22 01:59:35 +0700

    Update main.tf

--- my-terraform-project/main.tf
+++ my-terraform-project/main.tf
@@ -3,8 +3,9 @@ provider "google" {
   region  = var.region
 }
 
-data "google_compute_network" "existing_network" {
-  name    = "fppsomlops-network"
+data "google_compute_instance" "existing_instance" {
+  name    = "fppsomlops-instance"
+  zone    = var.default_zone
   project = var.project_id
 }
 
@@ -12,6 +13,8 @@ resource "google_compute_instance" "vm_instance" {
   name         = "fppsomlops-instance"
   machine_type = var.instance_type
   zone         = var.default_zone
+
+  count = length(data.google_compute_instance.existing_instance) == 0 ? 1 : 0
 
   boot_disk {
     initialize_params {
@@ -21,7 +24,7 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = data.google_compute_network.existing_network.name
+    network = google_compute_network.vpc_network.name
     access_config {}
   }
 
@@ -36,12 +39,19 @@ resource "google_compute_instance" "vm_instance" {
 }
 
 output "instance_ip" {
-  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.vm_instance[0].network_interface[0].access_config[0].nat_ip
+}
+
+data "google_compute_firewall" "existing_firewall" {
+  name    = "allow-http-https-ssh"
+  project = var.project_id
 }
 
 resource "google_compute_firewall" "default" {
   name    = "allow-http-https-ssh"
-  network = data.google_compute_network.existing_network.name
+  network = google_compute_network.vpc_network.name
+
+  count = length(data.google_compute_firewall.existing_firewall) == 0 ? 1 : 0
 
   allow {
     protocol = "tcp"
