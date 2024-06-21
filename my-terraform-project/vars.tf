592ccc599058120b066e1ad207b033c9455d5221
ndtsaniyah <90408425+ndtsaniyah@users.noreply.github.com>
2024-06-22 00:31:21 +0700

    Update vars.tf

--- my-terraform-project/vars.tf
+++ my-terraform-project/vars.tf
@@ -1,7 +1,7 @@
 variable "credentials_file" {
   description = "Path to the service account key file"
   type        = string
-  default     = "~/.gcp/gcp-key.json"
+  default     = ""
 }
 
 variable "project_id" {
